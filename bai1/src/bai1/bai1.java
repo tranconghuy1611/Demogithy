3ae797ba9017b31ebecbb880eab0dc0a6f658bfb
tranconghuy1611 <huy210105@gmail.com>
2024-09-18 10:58:51 +0700

    them bai1

---
+++ bai1/src/bai1/bai1.java
@@ -0,0 +1,12 @@
+package bai1;
+import java.util.Scanner;
+public class bai1 {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+         
+      String a;
+        System.out.println("nhap ten:");
+        a= scanner.next();
+        System.out.println("Hello "+a);
+    }
+}
